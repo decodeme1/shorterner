125efa44d51e8808a29a11cd6885218778dcfce0
decodeme1 <joaonascimento99gz@gmail.com>
2021-09-19 22:13:33 -0300

    ajustes

--- src/main/java/com/shorterner/app/repository/LinkRepository.java
+++ src/main/java/com/shorterner/app/repository/LinkRepository.java
@@ -3,5 +3,5 @@ package com.shorterner.app.repository;
 import com.shorterner.app.entity.Link;
 import org.springframework.data.repository.CrudRepository;
 
-public interface LinkRepository extends CrudRepository<gi, Long> {
+public interface LinkRepository extends CrudRepository<Link, Long> {
 }
